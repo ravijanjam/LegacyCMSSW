d6dd69e161cfa56a633b065604bdade6ca9b93fa
Eric <eric.appelt@gmail.com>
2015-02-10 15:31:34 -0600

    overhaul of the QCDAnalyzer code. May not currently work

--- src/QCDAnalyzer.cc
+++ src/QCDAnalyzer.cc
@@ -5,7 +5,7 @@
 // 
 /**\class QCDAnalyzer QCDAnalyzer.cc Appeltel/QCDAnalyzer/src/QCDAnalyzer.cc
 
- Description: [one line class summary]
+ Description: Gen-Level analyzer for QCD processes (i.e. jets, charged particles, parton flavor)
 
  Implementation:
      [Notes on implementation]
@@ -77,29 +77,34 @@ class QCDAnalyzer : public edm::EDAnalyzer {
       virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
 
       bool isDSEvent( const edm::Event&, const edm::EventSetup& );
+      bool isInEtaRange( const reco::Candidate&, double, double ); 
+      bool isInFlavor( const reco::MatchedPartons& ); 
+      bool isInSpecies( const reco::GenParticle& ); 
 
       // ----------member data ---------------------------
 
       edm::InputTag genJetSrc_;
       edm::InputTag genParticleSrc_;
-      bool doCMatrix_;
       bool doFlavor_;
+      bool doSpecies_;
+      bool onlyDS_;
       edm::InputTag flavorSrc_;
       std::vector<int> flavorId_;
-      bool jetsByAbsRapidity_;
-      double etaMin_;
-      double etaMax_;
+      std::vector<int> speciesId_;
+      bool useRapidity_;
+      double jetEtaMin_;
+      double jetEtaMax_;
+      double hEtaMin_;
+      double hEtaMax_;
       double jetRadius_;
       double pthatMin_;
       double pthatMax_;
-      std::vector<double> ptBins_;
-      std::string pythiaProcess_;
-      std::vector<double> dijetEtaBins_;
-      std::vector<double> dijetEtaWeights_;
+      std::vector<double> jetPtBins_;
+      std::vector<double> hPtBins_;
+      std::vector<double> etaBins_;
 
       std::map<std::string,TH1F*> hist_;
       std::map<std::string,TH2F*> hist2D_;
-
 };
 
 
@@ -110,82 +115,80 @@ class QCDAnalyzer : public edm::EDAnalyzer {
 QCDAnalyzer::QCDAnalyzer(const edm::ParameterSet& iConfig):
 genJetSrc_(iConfig.getParameter<edm::InputTag>("genJetSrc")),
 genParticleSrc_(iConfig.getParameter<edm::InputTag>("genParticleSrc")),
-doCMatrix_(iConfig.getParameter<bool>("doCMatrix")),
 doFlavor_(iConfig.getParameter<bool>("doFlavor")),
+doSpecies_(iConfig.getParameter<bool>("doSpecies")),
+onlyDS_(iConfig.getParameter<bool>("onlyDS")),
 flavorSrc_(iConfig.getParameter<edm::InputTag>("flavorSrc")),
 flavorId_(iConfig.getParameter<std::vector<int> >("flavorId")),
-jetsByAbsRapidity_(iConfig.getParameter<bool>("jetsByAbsRapidity")),
-etaMin_(iConfig.getParameter<double>("etaMin")),
-etaMax_(iConfig.getParameter<double>("etaMax")),
+speciesId_(iConfig.getParameter<std::vector<int> >("speciesId")),
+useRapidity_(iConfig.getParameter<bool>("useRapidity")),
+jetEtaMin_(iConfig.getParameter<double>("etaMin")),
+jetEtaMax_(iConfig.getParameter<double>("etaMax")),
+hEtaMin_(iConfig.getParameter<double>("etaMin")),
+hEtaMax_(iConfig.getParameter<double>("etaMax")),
 jetRadius_(iConfig.getParameter<double>("jetRadius")),
 pthatMin_(iConfig.getParameter<double>("pthatMin")),
 pthatMax_(iConfig.getParameter<double>("pthatMax")),
-ptBins_(iConfig.getParameter<std::vector<double> >("ptBins")),
-pythiaProcess_(iConfig.getParameter<std::string>("pythiaProcess")),
-dijetEtaBins_(iConfig.getParameter<std::vector<double> >("dijetEtaBins")),
-dijetEtaWeights_(iConfig.getParameter<std::vector<double> >("dijetEtaWeights"))
+jetPtBins_(iConfig.getParameter<std::vector<double> >("jetPtBins")),
+hPtBins_(iConfig.getParameter<std::vector<double> >("hPtBins")),
+etaBins_(iConfig.getParameter<std::vector<double> >("etaBins"))
 {
    edm::Service<TFileService> fs;
 
-   hist_["spectrum"] = fs->make<TH1F>("spectrum",";p_{T};counts",
-                           ptBins_.size()-1, &ptBins_[0]);
-   hist_["flavorspectrum"] = fs->make<TH1F>("flavorspectrum",";p_{T};counts",
-                           ptBins_.size()-1, &ptBins_[0]);
-   hist_["pspectrum"] = fs->make<TH1F>("pspectrum",";p_{T};counts",
-                           ptBins_.size()-1, &ptBins_[0]);
-   hist_["spectrum_DS"] = fs->make<TH1F>("spectrum_DS",";p_{T};counts",
-                           ptBins_.size()-1, &ptBins_[0]);
-   hist_["pspectrum_DS"] = fs->make<TH1F>("pspectrum_DS",";p_{T};counts",
-                           ptBins_.size()-1, &ptBins_[0]);
-   hist_["spectrum_fine"] = fs->make<TH1F>("spectrum_fine",";p_{T};counts",
-                           1000,0.,1000.);
-   hist_["flavorspectrum_fine"] = fs->make<TH1F>("flavorspectrum_fine",";p_{T};counts",
-                           100,0.,1000.);
-   hist_["pspectrum_fine"] = fs->make<TH1F>("pspectrum_fine",";p_{T};counts",
-                           1000,0.2,200.);
-   hist_["spectrum_dijet"] = fs->make<TH1F>("spectrum_dijet",";p_{T};counts",
-                           ptBins_.size()-1, &ptBins_[0]);
-   hist_["pspectrum_dijet"] = fs->make<TH1F>("pspectrum_dijet",";p_{T};counts",
-                           ptBins_.size()-1, &ptBins_[0]);
-   hist_["spectrum_dijetRW"] = fs->make<TH1F>("spectrum_dijetRW",";p_{T};counts",
-                           ptBins_.size()-1, &ptBins_[0]);
-   hist_["pspectrum_dijetRW"] = fs->make<TH1F>("pspectrum_dijetRW",";p_{T};counts",
-                           ptBins_.size()-1, &ptBins_[0]);
+   // Spectra histograms for eta (or y) within specified range
 
+   // jet spectrum
+   hist_["jetspectrum"] = fs->make<TH1F>("jetspectrum",";p_{T};counts",
+                           jetPtBins_.size()-1, &jetPtBins_[0]);
+   // hadron spectrum
+   hist_["hspectrum"] = fs->make<TH1F>("hspectrum",";p_{T};counts",
+                           hPtBins_.size()-1, &hPtBins_[0]);
+   // charged hadron spectrum
+   hist_["chspectrum"] = fs->make<TH1F>("chspectrum",";p_{T};counts",
+                           hPtBins_.size()-1, &hPtBins_[0]);
+
+   // positively charged hadron spectrum
+   hist_["pchspectrum"] = fs->make<TH1F>("pchspectrum",";p_{T};counts",
+                           hPtBins_.size()-1, &hPtBins_[0]);
+
+
+   // 2D histograms of (eta[y],pT) as above
+   hist2D_["jetspectrum2D"] = fs->make<TH2F>("jetspectrum2D",";p_{T};counts",
+                           etaBins_.size()-1, &etaBins_[0],
+                           jetPtBins_.size()-1, &jetPtBins_[0]);
+   hist2D_["hspectrum2D"] = fs->make<TH2F>("hspectrum2D",";p_{T};counts",
+                           etaBins_.size()-1, &etaBins_[0],
+                           hPtBins_.size()-1, &hPtBins_[0]);
+   hist2D_["chspectrum2D"] = fs->make<TH2F>("chspectrum2D",";p_{T};counts",
+                           etaBins_.size()-1, &etaBins_[0],
+                           hPtBins_.size()-1, &hPtBins_[0]);
+   hist2D_["pchspectrum2D"] = fs->make<TH2F>("pchspectrum2D",";p_{T};counts",
+                           etaBins_.size()-1, &etaBins_[0],
+                           hPtBins_.size()-1, &hPtBins_[0]);
+
+   // pt-hat or momentum transfer scale of PYTHIA process
+   // this will be 0 for diffractive events
    hist_["qscale"] = fs->make<TH1F>("qscale",";p_{T}-hat;counts",
                            1000,0.,1000.);
 
+   // leading charged hadron in approximate tracker acceptance (|eta|<2.5)
    hist_["lead_track"] = fs->make<TH1F>("lead_track",";p_{T};counts",
-                           1000,0.,1000.);
+                           hPtBins_.size()-1, &hPtBins_[0]);
 
+   // number of recorded events
    hist_["events"] = fs->make<TH1F>("events",";;events",1,0.,2.);
-   hist_["events_DS"] = fs->make<TH1F>("events_DS",";;double sided events",1,0.,2.);
 
-   hist2D_["cmatrix"] = fs->make<TH2F>("cmatrix",";p_{T} Jet;p_{T} h^{#pm}",
-                           1000,0.,1000.,1000,0.,200.);
-
-
-   hist2D_["flavormatrix"] = fs->make<TH2F>("flavormatrix",";p_{T} Jet;p_{T} h^{#pm}",
-                           100,0.,1000.,100,0.,200.);
-
-   hist2D_["flavormatrixpos"] = fs->make<TH2F>("flavormatrixpos",";p_{T} Jet;p_{T} h^{#pm}",
-                           100,0.,1000.,100,0.,200.);
-
-   hist2D_["flavormatrixneg"] = fs->make<TH2F>("flavormatrixneg",";p_{T} Jet;p_{T} h^{#pm}",
-                           100,0.,1000.,100,0.,200.);
-
-   // logarthmically binned spectra, delta pt = 1%
-
-   std::vector<double> logbins; logbins.reserve(2000); logbins.push_back(0);
-   for( double bin = 0.1; bin < 1000; bin += bin*0.1)
-     logbins.push_back(bin);
-   hist_["spectrum_log"] = fs->make<TH1F>("spectrum_log",";counts;p_{T}",
-                           logbins.size()-1, &logbins[0]);
-   hist_["pspectrum_log"] = fs->make<TH1F>("pspectrum_log",";counts;p_{T}",
-                           logbins.size()-1, &logbins[0]);
-
-  
-   std::cout << "Pythia process: " << pythiaProcess_ << std::endl;
+   // fragmentation function matrix - i.e. 2D histogram of hadrons vs jets
+   // also for just charged and positively charged hadrons
+   hist2D_["ffmatrix"] = fs->make<TH2F>("ffmatrix",";p_{T} Jet;p_{T} h^{#pm}",
+                           hPtBins_.size()-1, &hPtBins_[0],
+                           jetPtBins_.size()-1, &jetPtBins_[0]);
+   hist2D_["cffmatrix"] = fs->make<TH2F>("cffmatrix",";p_{T} Jet;p_{T} h^{#pm}",
+                           hPtBins_.size()-1, &hPtBins_[0],
+                           jetPtBins_.size()-1, &jetPtBins_[0]);
+   hist2D_["pcffmatrix"] = fs->make<TH2F>("pcffmatrix",";p_{T} Jet;p_{T} h^{#pm}",
+                           hPtBins_.size()-1, &hPtBins_[0],
+                           jetPtBins_.size()-1, &jetPtBins_[0]);
 
 }
 
@@ -218,205 +221,131 @@ QCDAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
      if( genEvtInfo->qScale() > pthatMax_ ) return;
    }
 
-   // Check if the event is DS
-   bool isDS = isDSEvent(iEvent,iSetup);
+   // Check if the event is DS and skip if configured
+   if( ! isDSEvent(iEvent,iSetup) && onlyDS_ ) return;
+  
+    Handle<reco::GenParticleCollection> pcol;
+   iEvent.getByLabel(genParticleSrc_,pcol);
 
    Handle<reco::GenJetCollection> gcol;
-   iEvent.getByLabel(genJetSrc_,gcol);
+   if( !doFlavor_ ) iEvent.getByLabel(genJetSrc_,gcol);
 
-   Handle<reco::GenParticleCollection> pcol;
-   iEvent.getByLabel(genParticleSrc_,pcol);
-   
    Handle<reco::JetMatchedPartonsCollection> fcol;
    if( doFlavor_) iEvent.getByLabel(flavorSrc_,fcol);
 
    hist_["events"]->Fill(1);
-   if(isDS) hist_["events_DS"]->Fill(1);
    hist_["qscale"]->Fill(genEvtInfo->qScale());
 
-
-
    // genjet spectra
-   for( const auto & jet : *gcol )
-   {
-     bool isInRange = false;
-
-     if( jetsByAbsRapidity_ == false &&  jet.eta() <= etaMax_ && jet.eta() >= etaMin_ )
-       isInRange = true;
-
-     if( jetsByAbsRapidity_ == true &&  fabs(jet.y()) <= etaMax_ && fabs(jet.y()) >= etaMin_ )
-       isInRange = true;
-
-     if( isInRange )
-     {
-       hist_["spectrum"]->Fill(jet.pt());
-       if(isDS) hist_["spectrum_DS"]->Fill(jet.pt());
-       hist_["spectrum_fine"]->Fill(jet.pt());
-       hist_["spectrum_log"]->Fill(jet.pt());
-     }
-   }
-
-   // flavor selected genjet spectra
-   if( doFlavor_) 
+   if ( doFlavor_ )
    {
      for( const auto & mjp : *fcol )
      {
+        if( ! isInFlavor( mjp.second ) ) continue;
         const reco::Jet *aJet = mjp.first.get();
-        const reco::MatchedPartons aMatch = mjp.second;
-
-        bool isInRange = false;
-        bool isInFlavor = false;
-        int flavor = 0;
-
-        if( jetsByAbsRapidity_ == false &&  aJet->eta() <= etaMax_ && aJet->eta() >= etaMin_ )
-          isInRange = true;
-        if( jetsByAbsRapidity_ == true &&  fabs(aJet->y()) <= etaMax_ && fabs(aJet->y()) >= etaMin_ )
-          isInRange = true;
-
-        if( aMatch.physicsDefinitionParton().isNonnull() )
-          flavor = aMatch.physicsDefinitionParton().get()->pdgId(); 
-        
-
-        for( const auto inFlavor : flavorId_ )
-        {
-          if( flavor == inFlavor ) isInFlavor = true;
-        } 
-      
-        if( isInRange && isInFlavor )
-        {
-          hist_["flavorspectrum"]->Fill(aJet->pt());
-          hist_["flavorspectrum_fine"]->Fill(aJet->pt());
-        }
+        if( isInEtaRange( *aJet, jetEtaMin_, jetEtaMax_ ) ) hist_["jetspectrum"]->Fill(aJet->pt());
+        hist2D_["jetspectrum2D"]->Fill(aJet->eta(),aJet->pt());
      }
-   } 
+   }
+   else
+   {
+     for( const auto & jet : *gcol )
+     {
+       if( isInEtaRange( jet, jetEtaMin_, jetEtaMax_ ) ) hist_["jetspectrum"]->Fill(jet.pt());
+       hist2D_["jetspectrum2D"]->Fill(jet.eta(),jet.pt());
+     }
+   }
 
-   // charged particle spectra and convolution matrix
+
+   // charged particle spectra and ff matrix
    double lead_track_pt = 0.0;
    for( const auto & h : *pcol )
    {
-     // skip decayed and neutral particles
-     if( h.status() != 1 || h.charge() == 0  ) continue;
+     // skip decayed  particles
+     if( h.status() != 1  ) continue;
 
-     if( fabs(h.eta()) < 2.5 && lead_track_pt < h.pt() )
+     // update leading track
+     if( h.charge() != 0 && fabs(h.eta()) < 2.5 && lead_track_pt < h.pt() )
        lead_track_pt = h.pt(); 
 
-     if( h.eta() <= etaMax_ - jetRadius_ && h.eta() >= etaMin_ + jetRadius_ )
+     if( doSpecies_ && ! isInSpecies(h) ) continue;
+       
+     hist2D_["hspectrum2D"]->Fill(h.eta(),h.pt());
+     if( h.charge() != 0 ) hist2D_["chspectrum2D"]->Fill(h.eta(),h.pt());
+     if( h.charge() > 0 ) hist2D_["pchspectrum2D"]->Fill(h.eta(),h.pt());
+
+     if( isInEtaRange(h, hEtaMin_, hEtaMax_) )
      {
-       hist_["pspectrum"]->Fill(h.pt());
-       if(isDS) hist_["pspectrum_DS"]->Fill(h.pt());
-       hist_["pspectrum_fine"]->Fill(h.pt());
-       hist_["pspectrum_log"]->Fill(h.pt());
-     
+       hist_["hspectrum"]->Fill(h.pt());
+       if( h.charge() != 0 ) hist_["chspectrum"]->Fill(h.pt());
+       if( h.charge() > 0 ) hist_["pchspectrum"]->Fill(h.pt());
+
        // associate with the highest-pt jet for which 
        // the track is found in the jet cone
-       if( doCMatrix_) 
-       {
-         double maxPtJet = 0.;
-         for( const auto & jet : *gcol )
-         {
-           if( jet.eta() <= etaMax_ && jet.eta() >= etaMin_ )
-           {
-             double dr = deltaR(jet,h);
-             if( dr < jetRadius_ && jet.pt() > maxPtJet)
-              maxPtJet = jet.pt();
-           }
-         }
-         hist2D_["cmatrix"]->Fill( maxPtJet, h.pt());
-       }
-
-       // associate with the highest-pt flavored jet for which 
-       // the track is found in the flavored jet cone
+     
+       double maxPtJet = 0.;
        if( doFlavor_)
        {
-         double maxPtJet = 0;
          for( const auto & mjp : *fcol )
          {
            const reco::Jet *aJet = mjp.first.get();
-           const reco::MatchedPartons aMatch = mjp.second;
-           bool isInRange = false;
-           bool isInFlavor = false;
-           int flavor = 0;
-
-           if(  aJet->eta() <= etaMax_ && aJet->eta() >= etaMin_ ) isInRange = true;
-           if( aMatch.physicsDefinitionParton().isNonnull() )
-             flavor = aMatch.physicsDefinitionParton().get()->pdgId();
-           for( const auto inFlavor : flavorId_ )
-           {
-             if( flavor == inFlavor ) isInFlavor = true;
-           }
-
-           if( isInRange && isInFlavor )
-           {
-             double dr = deltaR(*aJet,h);
-             if( dr < jetRadius_ && aJet->pt() > maxPtJet)
-               maxPtJet = aJet->pt();
-           }
+           if ( !isInEtaRange(*aJet,jetEtaMin_,jetEtaMax_) ) continue;
+           if( !isInFlavor( mjp.second ) ) continue;
+           double dr = deltaR(*aJet,h);
+           if( dr < jetRadius_ && aJet->pt() > maxPtJet)
+             maxPtJet = aJet->pt();
          }
-         hist2D_["flavormatrix"]->Fill( maxPtJet, h.pt());
-         if( h.charge() > 0 ) hist2D_["flavormatrixpos"]->Fill( maxPtJet, h.pt());
-         if( h.charge() < 0 ) hist2D_["flavormatrixneg"]->Fill( maxPtJet, h.pt());
        }
-
-
+       else
+       {
+         for( const auto & jet : *gcol )
+         {
+           if( ! isInEtaRange( jet, jetEtaMin_, jetEtaMax_ ) ) continue; 
+           double dr = deltaR(jet,h);
+           if( dr < jetRadius_ && jet.pt() > maxPtJet)
+             maxPtJet = jet.pt();
+         }
+       }
+       hist2D_["ffmatrix"]->Fill( maxPtJet, h.pt());
+       if( h.charge() != 0 ) hist2D_["cffmatrix"]->Fill( maxPtJet, h.pt());
+       if( h.charge() > 0 ) hist2D_["pcffmatrix"]->Fill( maxPtJet, h.pt());
      }
    }
 
    hist_["lead_track"]->Fill(lead_track_pt);
-   // determine dijet eta and fill related histograms
-   double leadJetPt = 0.0;
-   double leadJetPhi = 0.0;
-   double leadJetEta = 0.0;
-   for( const auto & jet : *gcol )
-   {
-     if( fabs(jet.eta()) > 3.0 ) continue;
-     if( jet.pt() > leadJetPt )
-     {
-       leadJetPt = jet.pt();
-       leadJetEta = jet.eta();
-       leadJetPhi = jet.phi();
-     }
-   }
-   double subleadJetPt = 0.0;
-   double subleadJetEta = 0.0;
-   for( const auto & jet : *gcol )
-   {
-     if( fabs(jet.eta()) > 3.0 ) continue;
-     if( deltaPhi(jet.phi(),leadJetPhi) < 2.09439510 ) continue;
-     if( jet.pt() > subleadJetPt )
-     {
-       subleadJetPt = jet.pt();
-       subleadJetEta = jet.eta();
-     }
-   }
-   if( leadJetPt > 120.0 && subleadJetPt > 30.0 )
-   {
-     double dijetEta = (leadJetEta + subleadJetEta ) / 2.0;
-     double dijetWeight = 1.0;
-     for( unsigned int i=0; i<dijetEtaBins_.size()-1; i++)
-     {
-       if( dijetEta >= dijetEtaBins_[i] && dijetEta < dijetEtaBins_[i+1] )
-         dijetWeight = dijetEtaWeights_[i]; 
-     } 
-     for( const auto & jet : *gcol )
-     {
-       if(   jet.eta() <= etaMax_ && jet.eta() >= etaMin_ )
-       {
-         hist_["spectrum_dijet"]->Fill(jet.pt());
-         hist_["spectrum_dijetRW"]->Fill(jet.pt(),dijetWeight);
-       }
-     }
-     for( const auto & h : *pcol )
-     {
-       if( h.status() != 1 || h.charge() == 0  ) continue;
-       if( h.eta() <= etaMax_ && h.eta() >= etaMin_ )
-       {
-         hist_["pspectrum_dijet"]->Fill(h.pt());
-         hist_["pspectrum_dijetRW"]->Fill(h.pt(),dijetWeight);
-       }
-     }
-  }
 }
 
+
+bool 
+QCDAnalyzer::isInEtaRange( const reco::Candidate& c, double etaMin, double etaMax )
+{
+     if( useRapidity_ == false &&  c.eta() <= etaMax && c.eta() >= etaMin )
+       return true;
+     if( useRapidity_ == true &&  c.y() <= etaMax && c.y() >= etaMin )
+       return true;
+     return false;
+}
+
+bool
+QCDAnalyzer::isInFlavor( const reco::MatchedPartons & aMatch )
+{
+  int flavor = 0;
+  if( aMatch.physicsDefinitionParton().isNonnull() )
+    flavor = aMatch.physicsDefinitionParton().get()->pdgId();
+  for( const auto inFlavor : flavorId_ )
+  { if( flavor == inFlavor ) return true; }
+ 
+  return false;
+}
+
+bool 
+QCDAnalyzer::isInSpecies( const reco::GenParticle & h )
+{
+  for( const auto inSpecies : speciesId_ )
+  { if( h.pdgId() == inSpecies ) return true; }
+
+  return false;  
+}
 
 bool 
 QCDAnalyzer::isDSEvent( const edm::Event& iEvent, const edm::EventSetup& iSetup)
